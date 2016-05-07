b69d0d52faa4641dbc48bcfe13cf7bf034e14920
Rachel Prince <rachaprince@gmail.com>
2016-05-07 12:10:07 -0400

    JS working

--- js/scroll.js
+++ js/scroll.js
@@ -15,11 +15,47 @@ $(function() {
 });
 });
 
-// $(document).ready(function (e) {
+ $(document).ready(function (e) {
 
-//     $('a[href*="#"]:not([href="#"])').click(function (e) { //Relative ( to its parent) mouse position 
-//         var posX = $(this).position().left,
-//             posY = $(this).position().top;
-//       alert(posX +' ' + posY);
-//     });
-// });
+
+  //click event on a particular arrow
+   $('.menu-toggle').click(function (e) { //Relative ( to its parent) mouse position 
+      e.preventDefault();
+      posX=0; 
+      posY=0;
+      rightX=0;
+
+    // find the top left corner of the box
+         posX = $(this).parent().offset().left;
+            posY = $(this).parent().offset().top;
+     
+
+      rightX=posX+$(this).parent().width();
+       //alert(posX +' ' + posY + ' ' +rightX);
+  //find the right div
+
+  //create div, toggle to visible, set position and height
+  $("#rose-nav").offset({top:posY, left:posX}).css({
+        'display': 'block'});
+
+  //stop scrolling (freeze index), hide other buttons, 
+   $("#wrapper").toggleClass("toggled");
+
+   
+  });
+
+   $('.menu-reverse').click(function(e){
+
+    $("#wrapper").toggleClass("toggled");
+  //reset offset
+     $('#rose-nav').offset({left:0, top: 0}).css({
+        'display': 'none'});
+ 
+
+
+      });
+ 
+});
+   
+
+   // helper function to get an element's exact position
